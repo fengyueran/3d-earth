c201d3b117d23c452916476410a7c5df156497d8
xinghunm <316032603@qq.com>
2023-09-03 15:18:32 +0800

    add 3d sphere

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
+import floorWood from "./assets/floor-wood.jpg";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const createMesh = (geom: THREE.BufferGeometry, imageFile: string) => {
+  const texture = new THREE.TextureLoader().load(imageFile);
+  const mat = new THREE.MeshPhongMaterial();
+  mat.map = texture;
 
-export default App
+  const mesh = new THREE.Mesh(geom, mat);
+  return mesh;
+};
+
+const App = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    const scene = new THREE.Scene();
+
+    const container = containerRef.current!;
+    // create a camera, which defines where we're looking at.
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      container.clientWidth / container.clientHeight,
+      0.1,
+      1000
+    );
+
+    // create a render and set the size
+    const webGLRenderer = new THREE.WebGLRenderer();
+    webGLRenderer.setClearColor(new THREE.Color(0x000));
+    webGLRenderer.setSize(container.clientWidth, container.clientHeight);
+    webGLRenderer.shadowMap.enabled = true;
+
+    const sphere = createMesh(new THREE.SphereGeometry(5, 20, 20), floorWood);
+    scene.add(sphere);
+
+    // position and point the camera to the center of the scene
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 26;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    // add spotlight for the shadows
+    const ambiLight = new THREE.AmbientLight(0x141414);
+    scene.add(ambiLight);
+
+    const light = new THREE.DirectionalLight();
+    light.position.set(10, 300, 2000);
+    scene.add(light);
+
+    container.appendChild(webGLRenderer.domElement);
+    let step = 0;
+    function render() {
+      step += 0.01;
+      sphere.rotation.y = step;
+      requestAnimationFrame(render);
+      webGLRenderer.render(scene, camera);
+    }
+    render();
+  }, []);
+
+  return <div style={{ width: "800px", height: "400px" }} ref={containerRef} />;
+};
+
+export default App;
